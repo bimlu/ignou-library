c0d8aac756a858b459f30bf51ec6aaa6249bd6bc
Robin Rawat <robinrawat7017@gmail.com>
2021-09-03 16:39:17 +0530

    experipent in new folder server/

---
+++ server/src/models/Student.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+/**
+ * Student schema that has references to User, Programme, Course schemas
+ */
+const studentSchema = new Schema(
+  {
+    firstName: String,
+    middlename: String,
+    lastName: String,
+    enrollmentNumber: String,
+    enrollmentDate: Date,
+    dateOfBirth: Date,
+    mobileNumber: String,
+    email: { type: String, unique: true },
+    password: String,
+    address: String,
+    district: String,
+    state: String,
+    pincode: Number,
+    country: String,
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    programmes: [{ type: Schema.Types.ObjectId, ref: "Programme" }],
+    courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
+    studyCenter: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", schemaSchema);
