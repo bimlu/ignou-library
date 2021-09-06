45db53cd77efa0f72e8e4f9f15c0d73a26fed169
Robin Rawat <robinrawat7017@gmail.com>
2021-09-07 00:02:38 +0530

    making soss...

---
+++ server/src/utils/populateDB/course/soss.js
@@ -0,0 +1,967 @@
+export const soss = [
+  {
+    // 1
+    code: "",
+    title: "",
+  },
+  {
+    // 2
+    code: "",
+    title: "",
+  },
+  {
+    // 3
+    code: "",
+    title: "",
+  },
+  {
+    // 4
+    code: "",
+    title: "",
+  },
+  {
+    // 5
+    code: "",
+    title: "",
+  },
+  {
+    // 6
+    code: "",
+    title: "",
+  },
+  {
+    // 7
+    code: "",
+    title: "",
+  },
+  {
+    // 8
+    code: "",
+    title: "",
+  },
+  {
+    // 9
+    code: "",
+    title: "",
+  },
+  {
+    // 10
+    code: "",
+    title: "",
+  },
+  {
+    // 11
+    code: "",
+    title: "",
+  },
+  {
+    // 12
+    code: "",
+    title: "",
+  },
+  {
+    // 13
+    code: "",
+    title: "",
+  },
+  {
+    // 14
+    code: "",
+    title: "",
+  },
+  {
+    // 15
+    code: "",
+    title: "",
+  },
+  {
+    // 16
+    code: "",
+    title: "",
+  },
+  {
+    // 17
+    code: "",
+    title: "",
+  },
+  {
+    // 18
+    code: "",
+    title: "",
+  },
+  {
+    // 19
+    code: "",
+    title: "",
+  },
+  {
+    // 20
+    code: "",
+    title: "",
+  },
+  {
+    // 21
+    code: "",
+    title: "",
+  },
+  {
+    // 22
+    code: "",
+    title: "",
+  },
+  {
+    // 23
+    code: "",
+    title: "",
+  },
+  {
+    // 24
+    code: "",
+    title: "",
+  },
+  {
+    // 25
+    code: "",
+    title: "",
+  },
+  {
+    // 26
+    code: "",
+    title: "",
+  },
+  {
+    // 27
+    code: "",
+    title: "",
+  },
+  {
+    // 28
+    code: "",
+    title: "",
+  },
+  {
+    // 29
+    code: "",
+    title: "",
+  },
+  {
+    // 30
+    code: "",
+    title: "",
+  },
+  {
+    // 31
+    code: "",
+    title: "",
+  },
+  {
+    // 32
+    code: "",
+    title: "",
+  },
+  {
+    // 33
+    code: "",
+    title: "",
+  },
+  {
+    // 34
+    code: "",
+    title: "",
+  },
+  {
+    // 35
+    code: "",
+    title: "",
+  },
+  {
+    // 36
+    code: "",
+    title: "",
+  },
+  {
+    // 37
+    code: "",
+    title: "",
+  },
+  {
+    // 38
+    code: "",
+    title: "",
+  },
+  {
+    // 39
+    code: "",
+    title: "",
+  },
+  {
+    // 40
+    code: "",
+    title: "",
+  },
+  {
+    // 41
+    code: "",
+    title: "",
+  },
+  {
+    // 42
+    code: "",
+    title: "",
+  },
+  {
+    // 43
+    code: "",
+    title: "",
+  },
+  {
+    // 44
+    code: "",
+    title: "",
+  },
+  {
+    // 45
+    code: "",
+    title: "",
+  },
+  {
+    // 46
+    code: "",
+    title: "",
+  },
+  {
+    // 47
+    code: "",
+    title: "",
+  },
+  {
+    // 48
+    code: "",
+    title: "",
+  },
+  {
+    // 49
+    code: "",
+    title: "",
+  },
+  {
+    // 50
+    code: "",
+    title: "",
+  },
+  {
+    // 51
+    code: "",
+    title: "",
+  },
+  {
+    // 52
+    code: "",
+    title: "",
+  },
+  {
+    // 53
+    code: "",
+    title: "",
+  },
+  {
+    // 54
+    code: "",
+    title: "",
+  },
+  {
+    // 55
+    code: "",
+    title: "",
+  },
+  {
+    // 56
+    code: "",
+    title: "",
+  },
+  {
+    // 57
+    code: "",
+    title: "",
+  },
+  {
+    // 58
+    code: "",
+    title: "",
+  },
+  {
+    // 59
+    code: "",
+    title: "",
+  },
+  {
+    // 60
+    code: "",
+    title: "",
+  },
+  {
+    // 61
+    code: "",
+    title: "",
+  },
+  {
+    // 62
+    code: "",
+    title: "",
+  },
+  {
+    // 63
+    code: "",
+    title: "",
+  },
+  {
+    // 64
+    code: "",
+    title: "",
+  },
+  {
+    // 65
+    code: "",
+    title: "",
+  },
+  {
+    // 66
+    code: "",
+    title: "",
+  },
+  {
+    // 67
+    code: "",
+    title: "",
+  },
+  {
+    // 68
+    code: "",
+    title: "",
+  },
+  {
+    // 69
+    code: "",
+    title: "",
+  },
+  {
+    // 70
+    code: "",
+    title: "",
+  },
+  {
+    // 71
+    code: "",
+    title: "",
+  },
+  {
+    // 72
+    code: "",
+    title: "",
+  },
+  {
+    // 73
+    code: "",
+    title: "",
+  },
+  {
+    // 74
+    code: "",
+    title: "",
+  },
+  {
+    // 75
+    code: "",
+    title: "",
+  },
+  {
+    // 76
+    code: "",
+    title: "",
+  },
+  {
+    // 77
+    code: "",
+    title: "",
+  },
+  {
+    // 78
+    code: "",
+    title: "",
+  },
+  {
+    // 79
+    code: "",
+    title: "",
+  },
+  {
+    // 80
+    code: "",
+    title: "",
+  },
+  {
+    // 81
+    code: "",
+    title: "",
+  },
+  {
+    // 82
+    code: "",
+    title: "",
+  },
+  {
+    // 83
+    code: "",
+    title: "",
+  },
+  {
+    // 84
+    code: "",
+    title: "",
+  },
+  {
+    // 85
+    code: "",
+    title: "",
+  },
+  {
+    // 86
+    code: "",
+    title: "",
+  },
+  {
+    // 87
+    code: "",
+    title: "",
+  },
+  {
+    // 88
+    code: "",
+    title: "",
+  },
+  {
+    // 89
+    code: "",
+    title: "",
+  },
+  {
+    // 90
+    code: "",
+    title: "",
+  },
+  {
+    // 91
+    code: "",
+    title: "",
+  },
+  {
+    // 92
+    code: "",
+    title: "",
+  },
+  {
+    // 93
+    code: "",
+    title: "",
+  },
+  {
+    // 94
+    code: "",
+    title: "",
+  },
+  {
+    // 95
+    code: "",
+    title: "",
+  },
+  {
+    // 96
+    code: "",
+    title: "",
+  },
+  {
+    // 97
+    code: "",
+    title: "",
+  },
+  {
+    // 98
+    code: "",
+    title: "",
+  },
+  {
+    // 99
+    code: "",
+    title: "",
+  },
+  {
+    // 100
+    code: "",
+    title: "",
+  },
+  {
+    // 101
+    code: "",
+    title: "",
+  },
+  {
+    // 102
+    code: "",
+    title: "",
+  },
+  {
+    // 103
+    code: "",
+    title: "",
+  },
+  {
+    // 104
+    code: "",
+    title: "",
+  },
+  {
+    // 105
+    code: "",
+    title: "",
+  },
+  {
+    // 106
+    code: "",
+    title: "",
+  },
+  {
+    // 107
+    code: "",
+    title: "",
+  },
+  {
+    // 108
+    code: "",
+    title: "",
+  },
+  {
+    // 109
+    code: "",
+    title: "",
+  },
+  {
+    // 110
+    code: "",
+    title: "",
+  },
+  {
+    // 111
+    code: "",
+    title: "",
+  },
+  {
+    // 112
+    code: "",
+    title: "",
+  },
+  {
+    // 113
+    code: "",
+    title: "",
+  },
+  {
+    // 114
+    code: "",
+    title: "",
+  },
+  {
+    // 115
+    code: "",
+    title: "",
+  },
+  {
+    // 116
+    code: "",
+    title: "",
+  },
+  {
+    // 117
+    code: "",
+    title: "",
+  },
+  {
+    // 118
+    code: "",
+    title: "",
+  },
+  {
+    // 119
+    code: "",
+    title: "",
+  },
+  {
+    // 120
+    code: "",
+    title: "",
+  },
+  {
+    // 121
+    code: "",
+    title: "",
+  },
+  {
+    // 122
+    code: "",
+    title: "",
+  },
+  {
+    // 123
+    code: "",
+    title: "",
+  },
+  {
+    // 124
+    code: "",
+    title: "",
+  },
+  {
+    // 125
+    code: "",
+    title: "",
+  },
+  {
+    // 126
+    code: "",
+    title: "",
+  },
+  {
+    // 127
+    code: "",
+    title: "",
+  },
+  {
+    // 128
+    code: "",
+    title: "",
+  },
+  {
+    // 129
+    code: "",
+    title: "",
+  },
+  {
+    // 130
+    code: "",
+    title: "",
+  },
+  {
+    // 131
+    code: "",
+    title: "",
+  },
+  {
+    // 132
+    code: "",
+    title: "",
+  },
+  {
+    // 133
+    code: "",
+    title: "",
+  },
+  {
+    // 134
+    code: "",
+    title: "",
+  },
+  {
+    // 135
+    code: "",
+    title: "",
+  },
+  {
+    // 136
+    code: "",
+    title: "",
+  },
+  {
+    // 137
+    code: "",
+    title: "",
+  },
+  {
+    // 138
+    code: "",
+    title: "",
+  },
+  {
+    // 139
+    code: "",
+    title: "",
+  },
+  {
+    // 140
+    code: "",
+    title: "",
+  },
+  {
+    // 141
+    code: "",
+    title: "",
+  },
+  {
+    // 142
+    code: "",
+    title: "",
+  },
+  {
+    // 143
+    code: "",
+    title: "",
+  },
+  {
+    // 144
+    code: "",
+    title: "",
+  },
+  {
+    // 145
+    code: "",
+    title: "",
+  },
+  {
+    // 146
+    code: "",
+    title: "",
+  },
+  {
+    // 147
+    code: "",
+    title: "",
+  },
+  {
+    // 148
+    code: "",
+    title: "",
+  },
+  {
+    // 149
+    code: "",
+    title: "",
+  },
+  {
+    // 150
+    code: "",
+    title: "",
+  },
+  {
+    // 151
+    code: "",
+    title: "",
+  },
+  {
+    // 152
+    code: "",
+    title: "",
+  },
+  {
+    // 153
+    code: "",
+    title: "",
+  },
+  {
+    // 154
+    code: "",
+    title: "",
+  },
+  {
+    // 155
+    code: "",
+    title: "",
+  },
+  {
+    // 156
+    code: "",
+    title: "",
+  },
+  {
+    // 157
+    code: "",
+    title: "",
+  },
+  {
+    // 158
+    code: "",
+    title: "",
+  },
+  {
+    // 159
+    code: "",
+    title: "",
+  },
+  {
+    // 160
+    code: "",
+    title: "",
+  },
+  {
+    // 161
+    code: "",
+    title: "",
+  },
+  {
+    // 162
+    code: "",
+    title: "",
+  },
+  {
+    // 163
+    code: "",
+    title: "",
+  },
+  {
+    // 164
+    code: "",
+    title: "",
+  },
+  {
+    // 165
+    code: "",
+    title: "",
+  },
+  {
+    // 166
+    code: "",
+    title: "",
+  },
+  {
+    // 167
+    code: "",
+    title: "",
+  },
+  {
+    // 168
+    code: "",
+    title: "",
+  },
+  {
+    // 169
+    code: "",
+    title: "",
+  },
+  {
+    // 170
+    code: "",
+    title: "",
+  },
+  {
+    // 171
+    code: "",
+    title: "",
+  },
+  {
+    // 172
+    code: "",
+    title: "",
+  },
+  {
+    // 173
+    code: "",
+    title: "",
+  },
+  {
+    // 174
+    code: "",
+    title: "",
+  },
+  {
+    // 175
+    code: "",
+    title: "",
+  },
+  {
+    // 176
+    code: "",
+    title: "",
+  },
+  {
+    // 177
+    code: "",
+    title: "",
+  },
+  {
+    // 178
+    code: "",
+    title: "",
+  },
+  {
+    // 179
+    code: "",
+    title: "",
+  },
+  {
+    // 180
+    code: "",
+    title: "",
+  },
+  {
+    // 181
+    code: "",
+    title: "",
+  },
+  {
+    // 182
+    code: "",
+    title: "",
+  },
+  {
+    // 183
+    code: "",
+    title: "",
+  },
+  {
+    // 184
+    code: "",
+    title: "",
+  },
+  {
+    // 185
+    code: "",
+    title: "",
+  },
+  {
+    // 186
+    code: "",
+    title: "",
+  },
+  {
+    // 187
+    code: "",
+    title: "",
+  },
+  {
+    // 188
+    code: "",
+    title: "",
+  },
+  {
+    // 189
+    code: "",
+    title: "",
+  },
+  {
+    // 190
+    code: "",
+    title: "",
+  },
+  {
+    // 191
+    code: "",
+    title: "",
+  },
+  {
+    // 192
+    code: "",
+    title: "",
+  },
+  {
+    // 193
+    code: "",
+    title: "",
+  },
+];
