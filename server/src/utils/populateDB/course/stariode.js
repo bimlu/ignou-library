cdf17cb3da7e8bafb7eb5004158967fa042e40af
Robin Rawat <robinrawat7017@gmail.com>
2021-09-07 13:21:12 +0530

    done stariode..

---
+++ server/src/utils/populateDB/course/stariode.js
@@ -0,0 +1,43 @@
+// staff training and research insitute of distance Education
+export const stariode = [
+  {
+    // 1
+    code: "MDE-411",
+    title: "Growth & Philosophy of Distance Education",
+  },
+  {
+    // 2
+    code: "MDE-413",
+    title: "Learner Support Systems & Services",
+  },
+  {
+    // 3
+    code: "MDE-414",
+    title: "Management of Distance Education",
+  },
+  {
+    // 4
+    code: "MDE-415",
+    title: "Research for Distance Education",
+  },
+  {
+    // 5
+    code: "MDE-416",
+    title: "Curriculum Development for Distance Education",
+  },
+  {
+    // 6
+    code: "MDE-418",
+    title: "Educational Communication Technologies",
+  },
+  {
+    // 7
+    code: "MDE-419",
+    title: "Staff Training & Development in Distance Education",
+  },
+  {
+    // 8
+    code: "MDEP-420",
+    title: "Project Work",
+  },
+];
