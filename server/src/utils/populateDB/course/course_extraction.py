7b98786029cb8c005a768aebb6e8696533d9c988
Robin Rawat <robinrawat7017@gmail.com>
2021-09-09 07:36:40 +0530

    imp: added school field to all courses

--- server/src/utils/populateDB/course/course_extraction.py
+++ server/src/utils/populateDB/course/course_extraction.py
@@ -1,15 +1,22 @@
 import re
 
+from school_course_mapping import school_with_possible_courses
+
 with open('course_catalog.csv', 'r') as in_file:
   data = in_file.read()
 
 trimmed_data = data.replace(',', ' ')
-
 # To visualize: re.findall('\d+\.[^\d][\(\)\-\da-zA-Z]*', trimmed_data)
-
 trimmed_data = re.findall('\d+\.[^\d][\(\)\-\da-zA-Z]*[\(\)\"\'\sa-zA-Z]+', trimmed_data)
 
 course_list = []
+
+def get_school_code(course_code, school_courses):
+  for school_code in school_courses:
+    possible_course_codes = school_courses[school_code]
+    for possible_course_code in possible_course_codes:
+      if course_code.startswith(possible_course_code + '-'):
+        return school_code
 
 for line in trimmed_data:
   # remove whitespaces at begining and end
@@ -31,7 +38,8 @@ for line in trimmed_data:
   else:
     raise NameError('couldn\'t split line')    
   # print(course_code, course_title)
-  course_dict = { 'code': course_code, 'title': course_title, 'school': '6133a4bd89c5b7dc15bfaca6' }
+  school_code = get_school_code(course_code, school_with_possible_courses)
+  course_dict = { 'code': course_code, 'title': course_title, 'school': school_code }
   if course_dict not in course_list:
     course_list.append(str(course_dict))
     course_list.append(',\n')
