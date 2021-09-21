d27532944daf931a45100d9d203007c928bc6f13
Robin Rawat <robinrawat7017@gmail.com>
2021-09-21 11:31:18 +0530

    insert programmes data

---
+++ api/src/populateDB/programme/awarenessAndAppreciation.js
@@ -0,0 +1,22 @@
+const awarenessAndAppreciationProgrammes = [
+  {
+    code: "APDF",
+    title: "Awareness Programme on Dairy Farming",
+    name: "APDF",
+    fullName: "Awareness Programme on Dairy Farming",
+    degree: 1,
+    termType: 1,
+    termsCount: 6,
+    collegeCode: "IGNOU",
+  },
+  {
+    code: "ACPSD",
+    title: "Appreciation Course on Population and Sustainable Development",
+    name: "ACPSD",
+    fullName: "Appreciation Course on Population and Sustainable Development",
+    degree: 1,
+    termType: 1,
+    termsCount: 6,
+    collegeCode: "IGNOU",
+  },
+];
