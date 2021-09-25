b51f58d6b028dfdff94fed4899a8fe77c6b678a2
Robin Rawat <robinrawat7017@gmail.com>
2021-09-25 11:48:37 +0530

    add programme structure

---
+++ api/src/populateDB/regex/addProgrammStructure.js
@@ -0,0 +1,13 @@
+const find = `(collegeCode: "IGNOU",)`;
+
+const replace = `$1
+programmeStructure: [
+  { term: 1, courseCode: "", category: Category.NONE, credit: 4 },
+  { term: 1, courseCode: "", category: Category.NONE, credit: 4 },
+  { term: 1, courseCode: "", category: Category.NONE, credit: 4 },
+  { term: 1, courseCode: "", category: Category.NONE, credit: 4 },
+  { term: 1, courseCode: "", category: Category.NONE, credit: 4 },
+  { term: 1, courseCode: "", category: Category.NONE, credit: 4 },
+  { term: 1, courseCode: "", category: Category.NONE, credit: 4 },
+  { term: 1, courseCode: "", category: Category.NONE, credit: 4 },
+],`;
