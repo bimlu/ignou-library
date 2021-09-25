083619472c2d769ca0e14f816e86e2240c28eddb
Robin Rawat <robinrawat7017@gmail.com>
2021-09-25 16:25:14 +0530

    add programme structure

--- api/src/populateDB/programme/awarenessAndAppreciation.js
+++ api/src/populateDB/programme/awarenessAndAppreciation.js
@@ -1,4 +1,4 @@
-import { Degree } from "../../constants/types";
+import { Degree, TermType } from "../../constants/types";
 
 export const awarenessAndAppreciationProgrammes = [
   {
@@ -7,9 +7,25 @@ export const awarenessAndAppreciationProgrammes = [
     name: "APDF",
     fullName: "Awareness Programme on Dairy Farming",
     degree: Degree.awarenessAndAppreciation,
-    termType: 1,
-    termsCount: 6,
+    termType: TermType.Quarter,
+    termsCount: 1,
     collegeCode: "IGNOU",
+    programmeStructure: [
+      { term: 1, courseCode: "Module-1", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-2", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-3", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-4", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-5", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-6", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-7", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-8", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-9", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-10", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-11", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-12", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-13", category: Category.NONE, credit: 0 },
+      { term: 1, courseCode: "Module-14", category: Category.NONE, credit: 0 },
+    ],
   },
   {
     code: "ACPSD",
@@ -17,8 +33,8 @@ export const awarenessAndAppreciationProgrammes = [
     name: "ACPSD",
     fullName: "Appreciation Course on Population and Sustainable Development",
     degree: Degree.awarenessAndAppreciation,
-    termType: 1,
-    termsCount: 6,
+    termType: TermType.Quarter,
+    termsCount: 1,
     collegeCode: "IGNOU",
   },
 ];
