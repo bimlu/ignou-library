12b32ae4e1fd70cbf0733419ca1ea3351e90d914
Robin Rawat <robinrawat7017@gmail.com>
2021-09-28 20:14:39 +0530

    style scrollbar in virtual list

--- client/src/pages/Programme/Programmes2.js
+++ client/src/pages/Programme/Programmes2.js
@@ -10,7 +10,7 @@ import Head from "components/Head";
 import SolidCard from "components/Cards/SolidCard";
 import ScrollManager from "components/ScrollManager";
 import Box from "@material-ui/core/Box";
-import Divider from "@material-ui/core/Divider";
+// import Divider from "@material-ui/core/Divider";
 import ProgrammeFilter from "./ProgrammeFilter";
 
 import { EXPLORE_PAGE_CARDS_LIMIT } from "constants/DataLimit";
@@ -20,11 +20,25 @@ import { SET_EXPLORE_ROUTE } from "store/route";
 import * as Routes from "routes";
 import { FixedSizeList as List } from "react-window";
 import { DegreeType2 } from "constants/DegreeType";
+import { BOTTOM_NAV_HEIGHT, HEADER_HEIGHT } from "constants/Layout";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+  list: {
+    overflowX: "hidden",
+    "&::-webkit-scrollbar": {
+      width: "0.25em",
+    },
+  },
+  row: {
+    paddingRight: theme.spacing(0.6),
+  },
+}));
 /**
  * Programmes page
  */
 const Programmes = () => {
+  const classes = useStyles();
   const cardColors = ["#203f52", "#4d137f", "#002244", "#004953"];
 
   const [{ datatree }, dispatch] = useStore();
@@ -61,64 +75,70 @@ const Programmes = () => {
     dispatch({ type: SET_EXPLORE_ROUTE, payload: pathname + search + hash });
   }, []);
 
-  if (error) return "Please check your internet connection";
+  const renderContent = () => {
+    if (!data) {
+      return (
+        <CardsContainer>
+          {Array.from(new Array(parseInt(EXPLORE_PAGE_CARDS_LIMIT / 2))).map((_el, i) => (
+            <SolidCard key={i} loading={true} />
+          ))}
+        </CardsContainer>
+      );
+    }
 
-  if (!data) {
-    return (
-      <CardsContainer>
-        {Array.from(new Array(parseInt(EXPLORE_PAGE_CARDS_LIMIT / 2))).map((_el, i) => (
-          <SolidCard key={i} loading={true} />
-        ))}
-      </CardsContainer>
+    if (error) return "Please check your internet connection";
+
+    const { programmes, count } = data.getCollegeProgrammes;
+    if (!programmes.length > 0) return <Empty text="No programmes yet." />;
+    const filteredProgrammes = programmes.filter(
+      (programme) => degree === "all" || programme.degree === DegreeType2.indexOf(degree)
     );
-  }
-  const { programmes, count } = data.getCollegeProgrammes;
-  if (!programmes.length > 0) return <Empty text="No programmes yet." />;
-  const filteredProgrammes = programmes.filter(
-    (programme) => degree === "all" || programme.degree === DegreeType2.indexOf(degree)
-  );
 
-  const Row = ({ index, style }) => {
-    const i = index;
+    const Row = ({ index, style }) => {
+      const i = index;
 
-    const programme = filteredProgrammes[i];
+      const programme = filteredProgrammes[i];
+      return (
+        <div style={style} className={classes.row}>
+          <SolidCard
+            key={programme.id}
+            title={programme.name}
+            subtitle={programme.fullName}
+            image={programme.image}
+            color={cardColors[i % cardColors.length]}
+            url={`${Routes.COURSES}?collegeId=${collegeId}&collegeName=${collegeName}&programmeId=${programme.id}&programmeName=${programme.name}&termType=${programme.termType}&termsCount=${programme.termsCount}#term=all`}
+            studentData={`Students: ${programme.studentsCount}`}
+            otherData={`Courses: ${programme.coursesCount}`}
+          />
+        </div>
+      );
+    };
+
     return (
-      <div style={style}>
-        <SolidCard
-          key={programme.id}
-          title={programme.name}
-          subtitle={programme.fullName}
-          image={programme.image}
-          color={cardColors[i % cardColors.length]}
-          url={`${Routes.COURSES}?collegeId=${collegeId}&collegeName=${collegeName}&programmeId=${programme.id}&programmeName=${programme.name}&termType=${programme.termType}&termsCount=${programme.termsCount}#term=all`}
-          studentData={`Students: ${programme.studentsCount}`}
-          otherData={`Courses: ${programme.coursesCount}`}
-        />
-      </div>
+      <List
+        height={window.innerHeight - HEADER_HEIGHT - BOTTOM_NAV_HEIGHT - 28}
+        itemCount={filteredProgrammes.length || count}
+        itemSize={280}
+        width="100%"
+        className={classes.list}
+      >
+        {Row}
+      </List>
     );
   };
 
   return (
-    <>
+    <div>
       <Head title={`${collegeName.toUpperCase()}`} />
 
       <ScrollManager scrollKey={`${pathname}${search}`} />
 
       <ProgrammeFilter degreesCount={8} selectedDegree={degree} />
 
-      <Box m={1} mb={2}>
-        <Divider />
-      </Box>
+      <Box m={1} />
 
-      <List
-        height={window.visualViewport.height - 52 - 52}
-        itemCount={filteredProgrammes.length || count}
-        itemSize={300}
-        width="100%"
-      >
-        {Row}
-      </List>
-    </>
+      {renderContent()}
+    </div>
   );
 };
 
