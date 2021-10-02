1c2f0c41387581c3e235519fb7e332c4480f438b
Robin Rawat <robinrawat7017@gmail.com>
2021-10-02 13:15:13 +0530

    redesigning card

--- client/src/components/Cards/SolidCard.js
+++ client/src/components/Cards/SolidCard.js
@@ -7,6 +7,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Color from "color";
 import { Link } from "react-router-dom";
 import Skeleton from "@material-ui/lab/Skeleton";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 
 const useStyles = makeStyles((theme) => ({
   actionArea: {
@@ -26,25 +27,29 @@ const useStyles = makeStyles((theme) => ({
   }),
   content: () => {
     return {
-      minHeight: 120,
+      minHeight: 110,
       // backgroundColor: color,
       background: theme.palette.background.paper,
       padding: theme.spacing(1),
-      "& > *": {
-        whiteSpace: "nowrap",
-        overflow: "hidden",
-        textOverflow: "ellipsis",
-        marginLeft: theme.spacing(1),
-      },
+      // "& > *": {
+      //   whiteSpace: "nowrap",
+      //   overflow: "hidden",
+      //   textOverflow: "ellipsis",
+      //   marginLeft: theme.spacing(1),
+      // },
     };
   },
   image: {
     component: "img",
     height: 150,
   },
+  checkIcon: {
+    fontSize: theme.spacing(2.2),
+    marginLeft: theme.spacing(0.6),
+  },
 }));
 
-const SolidCard = ({ title, subtitle, image, color, url, loading, studentData, otherData }) => {
+const SolidCard = ({ title, subtitle, image, color, url, loading }) => {
   const classes = useStyles({ color: color });
 
   return loading ? (
@@ -54,13 +59,27 @@ const SolidCard = ({ title, subtitle, image, color, url, loading, studentData, o
       <Card className={classes.card}>
         <CardMedia className={classes.image} image={image} alt={title} />
         <CardContent className={classes.content}>
-          <Typography variant="h5">{title.toUpperCase()}</Typography>
+          <Typography variant="h5" component="span" gutterBottom={true}>
+            <b>{title}</b>
+          </Typography>
 
-          <Typography variant="body2">{subtitle}</Typography>
+          <CheckCircleIcon color="primary" className={classes.checkIcon} />
 
-          <Typography variant="caption">{studentData}</Typography>
+          <Typography variant="body1" gutterBottom={true} noWrap={true}>
+            <b>{subtitle}</b>
+          </Typography>
+
+          {/* <Typography variant="caption">{studentData}</Typography>
           {", "}
-          <Typography variant="caption">{otherData}</Typography>
+          <Typography variant="caption">{otherData}</Typography> */}
+
+          <Typography variant="body1" noWrap={true}>
+            <b>132 Credits{" ┆ "}6 Semesters</b>
+          </Typography>
+
+          {/* <Typography>
+            <b>SOH</b>(School of Humanities)
+          </Typography> */}
         </CardContent>
       </Card>
     </CardActionArea>
