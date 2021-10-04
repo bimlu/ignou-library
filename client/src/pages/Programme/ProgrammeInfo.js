fd4cbb893b42f8e556c62fd8c0fa9ceccb23d604
Robin Rawat <robinrawat7017@gmail.com>
2021-10-04 22:27:22 +0530

    remove console.log

--- client/src/pages/Programme/ProgrammeInfo.js
+++ client/src/pages/Programme/ProgrammeInfo.js
@@ -100,7 +100,7 @@ const ProgrammeInfo = ({ collegeId }) => {
   }
 
   const college = data.getCollege;
-  console.log(college);
+  // console.log(college);
 
   return (
     <Paper className={classes.paper} elevation={0}>
