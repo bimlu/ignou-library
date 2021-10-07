eb567d8fb18bab96d4860c21099364511f2ef27b
Robin Rawat <robinrawat7017@gmail.com>
2021-10-07 17:15:36 +0530

    retrieve blocks and save it to file

--- api/src/populateDB/course/study_material/study_material.py
+++ api/src/populateDB/course/study_material/study_material.py
@@ -1,6 +1,10 @@
 import urllib.request
 from bs4 import BeautifulSoup
-import re
+from out_data import courses
+import os
+import json
+
+print(len(courses))
 
 def get_courses(programme_link):
   courses = []
@@ -26,7 +30,13 @@ def get_blocks(course_link):
   item_list = soup.body.find_all('h4', attrs={'class' : 'list-group-item-heading'})
 
   for item in item_list:
-    [block_code, block_name] = item.text.strip().split(' ', 1)
+    block = item.text.strip().split(' ', 1)
+    if len(block) == 2:
+      [block_code, block_name] = block
+    elif len(block) == 1:
+      block_code = block[0]
+      block_name = ''
+    else: continue
     block_link = 'https://egyankosh.ac.in' + item.find('a').get('href')
     blocks.append((block_code, block_name, block_link))
 
@@ -52,3 +62,21 @@ def get_units(block_link):
 # print(get_courses('https://egyankosh.ac.in/handle/123456789/410'))
 # print(get_blocks('https://egyankosh.ac.in/handle/123456789/452'))
 # print(get_units('https://egyankosh.ac.in/handle/123456789/1600'))
+
+
+save_path = '/home/robin/projects/ignou-app/api/src/populateDB/course/study_material'
+file_name = 'course_with_blocks.json'
+file_path = os.path.join(save_path, file_name)
+with open(file_path, 'w') as file: 
+  file.write('[\n')
+  count = 1
+  for course in courses:
+    print('extracting', count, 'course...')
+    count += 1
+    blocks = get_blocks(course["courseLink"])
+    course["blocks"] = blocks
+    file.writelines(json.dumps(course) + ',\n')
+  file.write(']')
+
+  
+
