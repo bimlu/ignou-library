a5718317e92373d340c9a85bcc1884b89807dc07
Robin Rawat <robinrawat7017@gmail.com>
2021-10-12 15:38:22 +0530

    nice header

--- client/src/components/App/Header/AppBar.js
+++ client/src/components/App/Header/AppBar.js
@@ -1,22 +1,11 @@
 import AppBar from "@material-ui/core/AppBar";
-import Badge from "@material-ui/core/Badge";
 import IconButton from "@material-ui/core/IconButton";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import AccountCircle from "@material-ui/icons/AccountCircle";
 import ToggleThemeIcon from "@material-ui/icons/Brightness4";
-import MailIcon from "@material-ui/icons/Mail";
-import MoreIcon from "@material-ui/icons/MoreVert";
-import NotificationsIcon from "@material-ui/icons/Notifications";
-import SignOut from "components/App/Header/SignOut";
 import SiteInfo from "constants/SiteInfo.json";
 import React from "react";
-import { generatePath, Link } from "react-router-dom";
-import * as Routes from "routes";
-import { useStore } from "store";
 import HideOnScroll from "./HideOnScroll";
 
 const useStyles = makeStyles((theme) => ({
@@ -24,8 +13,8 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   toolBar: {
-    [theme.breakpoints.up("lg")]: {
-      width: theme.breakpoints.values.lg,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.breakpoints.values.sm,
       margin: "0 auto",
     },
   },
@@ -35,134 +24,10 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: "monospace",
     fontSize: "1.8rem",
   },
-  sectionDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex",
-    },
-  },
-  sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none",
-    },
-  },
 }));
 
 const Header = ({ toggleThemeMode }) => {
-  const [{ auth }] = useStore();
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = "menu";
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      {auth.user && (
-        <MenuItem
-          dense
-          onClick={handleMenuClose}
-          component={Link}
-          to={generatePath(Routes.USER_PROFILE, { id: auth.user.id })}
-        >
-          <IconButton aria-label="account of current user" color="inherit">
-            <AccountCircle />
-          </IconButton>
-          <p>Profile</p>
-        </MenuItem>
-      )}
-
-      {auth.user && <SignOut handleClick={handleMenuClose} />}
-
-      {!auth.user && (
-        <MenuItem dense onClick={handleMenuClose} component={Link} to={Routes.LOG_IN}>
-          <IconButton aria-label="sign in" color="inherit">
-            <AccountCircle />
-          </IconButton>
-          <p>Sign in</p>
-        </MenuItem>
-      )}
-    </Menu>
-  );
-
-  const mobileMenuId = "menu-mobile";
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      {auth.user && (
-        <MenuItem
-          dense
-          onClick={handleMobileMenuClose}
-          component={Link}
-          to={generatePath(Routes.MESSAGES, { userId: Routes.NEW_ID_VALUE })}
-        >
-          <IconButton aria-label="show new mails" color="inherit">
-            <Badge variant="dot" invisible={!auth.user.newConversations.length} color="secondary">
-              <MailIcon />
-            </Badge>
-          </IconButton>
-          <p>Messages</p>
-        </MenuItem>
-      )}
-
-      {auth.user && (
-        <MenuItem dense onClick={handleMobileMenuClose} component={Link} to={Routes.NOTIFICATIONS}>
-          <IconButton aria-label="show new notifications" color="inherit">
-            <Badge variant="dot" invisible={!auth.user.newNotifications.length} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <p>Notifications</p>
-        </MenuItem>
-      )}
-
-      {auth.user && <SignOut handleClick={handleMenuClose} />}
-
-      {!auth.user && (
-        <MenuItem dense onClick={handleMobileMenuClose} component={Link} to={Routes.LOG_IN}>
-          <IconButton aria-label="sign in" color="inherit">
-            <AccountCircle />
-          </IconButton>
-          <p>Sign in</p>
-        </MenuItem>
-      )}
-    </Menu>
-  );
 
   return (
     <div className={classes.grow}>
@@ -178,66 +43,11 @@ const Header = ({ toggleThemeMode }) => {
             <IconButton aria-label="toggle theme" onClick={toggleThemeMode} color="inherit">
               <ToggleThemeIcon />
             </IconButton>
-
-            <div className={classes.sectionDesktop}>
-              {auth.user && (
-                <IconButton
-                  aria-label="show new messages"
-                  color="inherit"
-                  component={Link}
-                  to={generatePath(Routes.MESSAGES, { userId: Routes.NEW_ID_VALUE })}
-                >
-                  <Badge variant="dot" invisible={!auth.user.newConversations.length} color="secondary">
-                    <MailIcon />
-                  </Badge>
-                </IconButton>
-              )}
-
-              {auth.user && (
-                <IconButton
-                  aria-label="show new notifications"
-                  color="inherit"
-                  component={Link}
-                  to={Routes.NOTIFICATIONS}
-                >
-                  <Badge variant="dot" invisible={!auth.user.newNotifications.length} color="secondary">
-                    <NotificationsIcon />
-                  </Badge>
-                </IconButton>
-              )}
-
-              <IconButton
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </div>
-
-            <div className={classes.sectionMobile}>
-              <IconButton
-                aria-label="show more"
-                aria-controls={mobileMenuId}
-                aria-haspopup="true"
-                onClick={handleMobileMenuOpen}
-                color="inherit"
-              >
-                <MoreIcon />
-              </IconButton>
-            </div>
           </Toolbar>
         </AppBar>
       </HideOnScroll>
 
       <Toolbar />
-
-      {renderMobileMenu}
-
-      {renderMenu}
     </div>
   );
 };
