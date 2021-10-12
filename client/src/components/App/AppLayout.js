a6d4b0e8492ec5df63f44cacb98523e4818c6755
Robin Rawat <robinrawat7017@gmail.com>
2021-10-12 11:59:10 +0530

    remove grid

--- client/src/components/App/AppLayout.js
+++ client/src/components/App/AppLayout.js
@@ -1,7 +1,4 @@
 import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import { useTheme } from "@material-ui/core/styles";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
 import BottomNavigation from "components/App/BottomNavigation";
 import Header from "components/App/Header";
 import { Spacing } from "components/Layout";
@@ -20,125 +17,104 @@ import { StudyMaterial } from "pages/StudyMaterial";
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import * as Routes from "routes";
-import { useStore } from "store";
-import SideBar from "./SideBar";
-import UserSuggestions from "./UserSuggestions";
 
 /**
  * Main layout of the app, when user is authenticated
  */
 const AppLayout = ({ toggleThemeMode }) => {
-  const [{ auth }] = useStore();
-  const theme = useTheme();
-  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
-
   return (
     <>
-      <Spacing top={isDesktop && "sm"} bottom="xl">
-        <Container>
-          <Grid container spacing={isDesktop ? 4 : 2}>
-            <Grid item xs={3}>
-              <SideBar />
-            </Grid>
+      <Spacing top="sm" bottom="xl">
+        <Container maxWidth="sm">
+          <Switch>
+            {/* College App List Pages */}
+            <Route exact path={Routes.COLLEGES}>
+              <Colleges />
+            </Route>
 
-            <Grid item xs={isDesktop ? 6 : 12}>
-              <Switch>
-                {/* College App List Pages */}
-                <Route exact path={Routes.COLLEGES}>
-                  <Colleges />
-                </Route>
+            <Route exact path={Routes.PROGRAMMES}>
+              <Programmes />
+            </Route>
 
-                <Route exact path={Routes.PROGRAMMES}>
-                  <Programmes />
-                </Route>
+            <Route exact path={Routes.COURSES}>
+              <Courses />
+            </Route>
 
-                <Route exact path={Routes.COURSES}>
-                  <Courses />
-                </Route>
+            <Route exact path={Routes.POSTS}>
+              {/* <Posts /> */}
+              <StudyMaterial />
+            </Route>
+            {/* *************** */}
 
-                <Route exact path={Routes.POSTS}>
-                  {/* <Posts /> */}
-                  <StudyMaterial />
-                </Route>
-                {/* *************** */}
+            {/* College App Detail pages */}
+            <Route exact path={Routes.COLLEGE}>
+              <College />
+            </Route>
 
-                {/* College App Detail pages */}
-                <Route exact path={Routes.COLLEGE}>
-                  <College />
-                </Route>
+            <Route exact path={Routes.PROGRAMME}>
+              <Programme />
+            </Route>
 
-                <Route exact path={Routes.PROGRAMME}>
-                  <Programme />
-                </Route>
+            <Route exact path={Routes.COURSE}>
+              <Course />
+            </Route>
 
-                <Route exact path={Routes.COURSE}>
-                  <Course />
-                </Route>
+            <Route exact path={Routes.POST}>
+              <Post />
+            </Route>
+            {/* *************** */}
 
-                <Route exact path={Routes.POST}>
-                  <Post />
-                </Route>
-                {/* *************** */}
+            {/* College App Create pages */}
+            <Route exact path={Routes.CREATE_COLLEGE}>
+              <CreateCollege />
+            </Route>
 
-                {/* College App Create pages */}
-                <Route exact path={Routes.CREATE_COLLEGE}>
-                  <CreateCollege />
-                </Route>
+            <Route exact path={Routes.CREATE_PROGRAMME}>
+              <CreateProgramme />
+            </Route>
 
-                <Route exact path={Routes.CREATE_PROGRAMME}>
-                  <CreateProgramme />
-                </Route>
+            <Route exact path={Routes.CREATE_COURSE}>
+              <CreateCourse />
+            </Route>
 
-                <Route exact path={Routes.CREATE_COURSE}>
-                  <CreateCourse />
-                </Route>
+            <Route exact path={Routes.CREATE_POST}>
+              <CreatePost />
+            </Route>
+            {/* **************** */}
 
-                <Route exact path={Routes.CREATE_POST}>
-                  <CreatePost />
-                </Route>
-                {/* **************** */}
+            {/* Main App pages*/}
+            <Route exact path={Routes.HOME}>
+              <Header toggleThemeMode={toggleThemeMode} />
+              <Home />
+            </Route>
 
-                {/* Main App pages*/}
-                <Route exact path={Routes.HOME}>
-                  <Header toggleThemeMode={toggleThemeMode} />
-                  <Home />
-                </Route>
+            <Route exact path={Routes.EXPLORE}>
+              <Explore />
+            </Route>
 
-                <Route exact path={Routes.EXPLORE}>
-                  <Explore />
-                </Route>
+            <Route exact path={Routes.PEOPLE}>
+              <People />
+            </Route>
 
-                <Route exact path={Routes.PEOPLE}>
-                  <People />
-                </Route>
+            <Route exact path={Routes.NOTIFICATIONS}>
+              <Notifications />
+            </Route>
 
-                <Route exact path={Routes.NOTIFICATIONS}>
-                  <Notifications />
-                </Route>
+            <Route exact path={Routes.MESSAGES}>
+              <Messages />
+            </Route>
 
-                <Route exact path={Routes.MESSAGES}>
-                  <Messages />
-                </Route>
+            <Route exact path={Routes.USER_PROFILE}>
+              <Profile />
+            </Route>
+            {/* ************* */}
 
-                <Route exact path={Routes.USER_PROFILE}>
-                  <Profile />
-                </Route>
-                {/* ************* */}
-
-                {/* Fallback page */}
-                <Route>
-                  <NotFound />
-                </Route>
-                {/* ************** */}
-              </Switch>
-            </Grid>
-
-            {auth.user && isDesktop && (
-              <Grid item xs={3}>
-                <UserSuggestions />
-              </Grid>
-            )}
-          </Grid>
+            {/* Fallback page */}
+            <Route>
+              <NotFound />
+            </Route>
+            {/* ************** */}
+          </Switch>
         </Container>
       </Spacing>
 
