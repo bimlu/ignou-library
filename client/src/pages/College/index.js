abf94a8d7a386e3a65198fa4938918d67de55114
Robin Rawat <robinrawat7017@gmail.com>
2021-10-12 17:46:05 +0530

    remove unwanted files

--- client/src/pages/College/index.js
+++
@@ -1,5 +0,0 @@
-import College from "./College";
-import Colleges from "./Colleges";
-import CreateCollege from "./CreateCollege";
-
-export { College, Colleges, CreateCollege };
--- client/src/pages/Post/index.js
+++
@@ -1,5 +0,0 @@
-import CreatePost from "./CreatePost";
-import Post from "./Post";
-import Posts from "./Posts";
-
-export { Post, Posts, CreatePost };
