fc85936bdb5904c502c1a62d16c8169d690d5102
Robin Rawat <robinrawat7017@gmail.com>
2021-10-17 20:35:11 +0530

    courses logo

--- client/src/pages/Programme/CourseFilter.js
+++ client/src/pages/Programme/CourseFilter.js
@@ -1,3 +1,4 @@
+import { Typography } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import { TermType } from "constants/TermType";
@@ -5,6 +6,12 @@ import React from "react";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    position: "sticky",
+    top: 0,
+    zIndex: theme.zIndex.appBar - 1,
+    background: theme.palette.background.default,
+  },
+  filterList: {
     paddingBottom: theme.spacing(1),
     paddingTop: theme.spacing(1),
     whiteSpace: "nowrap",
@@ -21,10 +28,6 @@ const useStyles = makeStyles((theme) => ({
     "&::-webkit-scrollbar": {
       height: "0.32em",
     },
-    position: "sticky",
-    top: 0,
-    zIndex: theme.zIndex.appBar - 1,
-    background: theme.palette.background.default,
   },
 }));
 
@@ -37,28 +40,34 @@ export default function CourseFilter({ termType, termsCount, selectedTerm }) {
 
   return (
     <div className={classes.root}>
-      <Button
-        variant={selectedTerm === "all" ? "contained" : "outlined"}
-        size="small"
-        color="primary"
-        onClick={() => handleClick("all")}
-      >
-        All
-      </Button>
+      <Typography variant="h3" color="textSecondary">
+        <b>Courses</b>
+      </Typography>
 
-      {Array.from(new Array(termsCount))
-        .map((_el, idx) => idx + 1)
-        .map((num) => (
-          <Button
-            variant={selectedTerm === String(num) ? "contained" : "outlined"}
-            size="small"
-            color="primary"
-            key={num}
-            onClick={() => handleClick(num)}
-          >
-            {`${TermType[termType]}-${num}`}
-          </Button>
-        ))}
+      <div className={classes.filterList}>
+        <Button
+          variant={selectedTerm === "all" ? "contained" : "outlined"}
+          size="small"
+          color="primary"
+          onClick={() => handleClick("all")}
+        >
+          All
+        </Button>
+
+        {Array.from(new Array(termsCount))
+          .map((_el, idx) => idx + 1)
+          .map((num) => (
+            <Button
+              variant={selectedTerm === String(num) ? "contained" : "outlined"}
+              size="small"
+              color="primary"
+              key={num}
+              onClick={() => handleClick(num)}
+            >
+              {`${TermType[termType]}-${num}`}
+            </Button>
+          ))}
+      </div>
     </div>
   );
 }
