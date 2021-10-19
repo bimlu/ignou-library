fa81c44888393253afc26aca895b67806cfd8f23
Robin Rawat <robinrawat7017@gmail.com>
2021-10-19 20:37:54 +0530

    delete unused files

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -3,18 +3,7 @@ import Course from "./Course";
 import Programme from "./Programme";
 
 export default {
-  // User,
-  // Follow,
-  // Post,
-  // Like,
-  // Comment,
-  // Message,
-  // Notification,
   College,
   Programme,
   Course,
-  // Faculty,
-  // School,
-  // IGNOU,
-  // Student,
 };
