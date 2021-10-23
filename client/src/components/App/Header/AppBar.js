a62318e0de839f87ba7fb42fd63506e9bb6d18e4
Robin Rawat <robinrawat7017@gmail.com>
2021-10-23 19:40:21 +0530

    don't hide on scroll

--- client/src/components/App/Header/AppBar.js
+++ client/src/components/App/Header/AppBar.js
@@ -1,13 +1,13 @@
 import AppBar from "@material-ui/core/AppBar";
 import IconButton from "@material-ui/core/IconButton";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import ToggleThemeIcon from "@material-ui/icons/Brightness4";
 import SiteInfo from "constants/SiteInfo.json";
 import React from "react";
-import HideOnScroll from "./HideOnScroll";
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -29,30 +29,30 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = ({ toggleThemeMode, showBackButton = false }) => {
   const classes = useStyles();
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("sm"));
 
   return (
     <div className={classes.grow}>
-      <HideOnScroll>
-        <AppBar position="fixed" color="inherit">
-          <Toolbar className={classes.toolBar}>
-            {showBackButton ? (
-              <IconButton aria-label="go back" onClick={() => history.back()}>
-                <ArrowBackIcon />
-              </IconButton>
-            ) : (
-              <Typography className={classes.title} variant="h6" noWrap>
-                {SiteInfo.name}
-              </Typography>
-            )}
-
-            <div className={classes.grow} />
-
-            <IconButton aria-label="toggle theme" onClick={toggleThemeMode} color="inherit">
-              <ToggleThemeIcon />
+      <AppBar position={isDesktop ? "fixed" : "absolute"} color="inherit">
+        <Toolbar className={classes.toolBar}>
+          {showBackButton ? (
+            <IconButton aria-label="go back" onClick={() => history.back()}>
+              <ArrowBackIcon />
             </IconButton>
-          </Toolbar>
-        </AppBar>
-      </HideOnScroll>
+          ) : (
+            <Typography className={classes.title} variant="h6" noWrap>
+              {SiteInfo.name}
+            </Typography>
+          )}
+
+          <div className={classes.grow} />
+
+          <IconButton aria-label="toggle theme" onClick={toggleThemeMode} color="inherit">
+            <ToggleThemeIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
 
       <Toolbar />
     </div>
