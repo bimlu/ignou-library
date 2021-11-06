42998792beccdd464d85da038065ce49d0671a02
Robin Rawat <robinrawat7017@gmail.com>
2021-11-06 14:52:38 +0530

    remove unwanted files

--- server/src/resolvers/index.js
+++
@@ -1,5 +0,0 @@
-import courseResolver from "./course";
-import programmeResolver from "./programme";
-import userResolver from "./user";
-
-export default [userResolver, courseResolver, programmeResolver];
--- server/src/utils/populateDB/index.js
+++
@@ -1,9 +0,0 @@
-import { createCourses } from "./course";
-import { createProgrammes } from "./programme/index";
-// import { createSchools } from "./school";
-
-export const populateDB = () => {
-  // createSchools();
-  createProgrammes();
-  createCourses();
-};
