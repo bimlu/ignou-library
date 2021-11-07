b4d71232a39157dbad50021a90987c4caa9f3bfd
Robin Rawat <robinrawat7017@gmail.com>
2021-11-07 07:50:28 +0530

    remove unwanted files

--- api/scripts/git-script.sh
+++
@@ -1,9 +0,0 @@
-cd ~/projects/jhimlish &&
-git checkout main &&
-git merge develop &&
-git checkout main-backup &&
-git merge develop &&
-git checkout develop &&
-git push origin --all &&
-git push origin --tags &&
-git log
