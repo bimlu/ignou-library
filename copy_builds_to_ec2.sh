762120df6cdd289708f1ecdce9282b77dc96e3f8
Robin Rawat <robinrawat7017@gmail.com>
2021-11-10 23:49:34 +0530

    fix

--- copy_builds_to_ec2.sh
+++ copy_builds_to_ec2.sh
@@ -1,7 +1,7 @@
 cd api && tsc &&
 scp -i ~/Documents/mern-app.pem  -r build/  ubuntu@library.ignouwala.com:/home/ubuntu/ignou-library/api &&
 cd ../client && && npm run build &&
-scp -i ~/Documents/mern-app.pem  -r .build/  ubuntu@library.ignouwala.com:/home/ubuntu/ignou-library/client &&
+scp -i ~/Documents/mern-app.pem  -r build/  ubuntu@library.ignouwala.com:/home/ubuntu/ignou-library/client &&
 echo "Done!" && 
 ssh -i  ~/Documents/mern-app.pem ubuntu@library.ignouwala.com
 # pm2 restart all # on server
