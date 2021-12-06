96475e4d5c7acbbf51ebbbfc309c4461c95df379
Robin Rawat <robinrawat7017@gmail.com>
2021-12-06 17:57:15 +0530

    initialize ignou-download

--- mongodump_to_ec2.sh
+++ mongodump_to_ec2.sh
@@ -1,3 +1,7 @@
 mongodump --archive="mongodump_ignou_library" --db="ignou-library" &&
 scp -i ~/Documents/mern-app.pem mongodump_ignou_library  ubuntu@library.ignouwala.com:/home/ubuntu/mongodump_ignou_library
 # mongorestore --archive="mongodump_ignou_library" # on server
+
+# new style
+mongodump --archive="mongodump_ignou_network" --db="ignou-network"
+mongorestore --archive="mongodump_ignou_network" --nsFrom='ignou-network.*' --nsTo='ignou-meet.*'
