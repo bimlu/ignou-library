7901878f6042bcbf167ba6f7abfd5f0f832b2df0
Robin Rawat <robinrawat7017@gmail.com>
2022-02-10 21:36:42 +0530

    update for new ec2 robinrawat7017 -> jhimlish

--- copy_builds_to_ec2.sh
+++ copy_builds_to_ec2.sh
@@ -1,7 +1,10 @@
 cd api && tsc &&
 scp -i ~/Documents/mern-app.pem  -r build/  ubuntu@library.ignouwala.com:/home/ubuntu/ignou-library/api &&
+# scp -i ../aws-jhimlish-for-dogehouse.pem  -r build/  ubuntu@52.66.166.69:/home/ubuntu/ignou-library/api &&
 cd ../client && && npm run build &&
 scp -i ~/Documents/mern-app.pem  -r build/  ubuntu@library.ignouwala.com:/home/ubuntu/ignou-library/client &&
+# scp -i ../aws-jhimlish-for-dogehouse.pem  -r build/  ubuntu@52.66.166.69:/home/ubuntu/ignou-library/client &&
 echo "Done!" && 
 ssh -i  ~/Documents/mern-app.pem ubuntu@library.ignouwala.com
+# ssh -i ../aws-jhimlish-for-dogehouse.pem ubuntu@52.66.166.69
 # pm2 restart all # on server
